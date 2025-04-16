76287bf03a9e203d26f0fa5ebf46bfec3c7e4834
Arne Duyver <61016144+ArneDuyver@users.noreply.github.com>
2025-04-16 13:04:43 +0200

    Update SpelerRepository.java

--- app/src/main/java/be/kuleuven/SpelerRepository.java
+++ app/src/main/java/be/kuleuven/SpelerRepository.java
@@ -18,7 +18,7 @@ public interface SpelerRepository {
   // Hierbij komt een finale plaats van 1 overeen met 'finale', 2 met 'halve finale', 4 met 'kwart finale', hoger dan 4 met 'lager dan kwart finale'.
   // LOT OP: indien de speler de finale ook gewonnen heeft is de finalestring = 'winst'
 
-  public void addSpelerToTornooi(int tornooiId);
+  public void addSpelerToTornooi(int tornooiId, int tennisvlaanderenId);
 
-  public void removeSpelerFromTornooi(int tornooiId);
+  public void removeSpelerFromTornooi(int tornooiId, int tennisvlaanderenId);
 }
